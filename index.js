0d622e2d03b795ebfc8d81d1112c3d073a6ed532
Ben Smerd <bsmerd@adam.com.au>
2022-12-09 17:23:59 +1030

    comment init

---
+++ index.js
@@ -0,0 +1,11 @@
+// similar to this --https://www.youtube.com/watch?v=fU4o_BKaUZE
+
+// * potentially like a user prompt story setter with AI images
+
+//* just a client side project
+
+//* prompt user- describe a person (blonde haired man), describe a setting (in a dark scary forest) and a doing thing (diving into a lake) and then generate the image
+
+// * Would be cool to try this with a python logic for the API
+//* Could try angular as well
+//* Learn how to connect python to front end JS
